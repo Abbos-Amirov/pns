24c1c017a1d98cee831553e7e0f3c5d745145c58
Abbos-Amirov <abbosamirov571@gmail.com>
2025-11-01 18:25:18 +0900

    feat: configure env variables

--- apps/pns-batch/src/main.ts
+++ apps/pns-batch/src/main.ts
@@ -3,6 +3,6 @@ import { PnsBatchModule } from './pns-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(PnsBatchModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
--- apps/pns-batch/src/pns-batch.module.ts
+++ apps/pns-batch/src/pns-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PnsBatchController } from './pns-batch.controller';
 import { PnsBatchService } from './pns-batch.service';
+import{ConfigModule} from "@nestjs/config"
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [PnsBatchController],
   providers: [PnsBatchService],
 })
