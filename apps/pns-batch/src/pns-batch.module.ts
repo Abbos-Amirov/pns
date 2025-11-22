e5c0dff90e71e87a83d05ff168d824e474e7b3cf
Abbos-Amirov <abbosamirov571@gmail.com>
2025-11-22 13:48:25 +0900

    feat: modify batch module and connect to MongoDb

--- apps/pns-batch/src/pns-batch.module.ts
+++ apps/pns-batch/src/pns-batch.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PnsBatchController } from './pns-batch.controller';
 import { PnsBatchService } from './pns-batch.service';
 import{ConfigModule} from "@nestjs/config"
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DatabaseModule],
   controllers: [PnsBatchController],
   providers: [PnsBatchService],
 })
